e2200df419cf285f135d652ee1858d630f932d33
TinyAllen <allen.wang.123@outlook.com>
2019-02-28 22:13:59 +0800

    Update

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,5 @@
 module.exports = {
-  presets: ['@vue/app'],
-  plugins: [
-    [
-      'import',
-      {
-        libraryName: 'iview',
-        libraryDirectory: 'src/components',
-      },
-    ],
-  ],
-};
+  presets: [
+    '@vue/app'
+  ]
+}
