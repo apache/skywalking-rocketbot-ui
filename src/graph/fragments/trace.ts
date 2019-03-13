285e3a874aed194f899c16f06306ec6a597dac8c
TinyAllen <allen.wang.123@outlook.com>
2019-03-13 13:29:56 +0800

    Change LICENSE to Apache 2.0

--- src/graph/fragments/trace.ts
+++ src/graph/fragments/trace.ts
@@ -1,18 +1,20 @@
 /**
- * "condition": {
- *   "traceState": "ALL",
- *   "queryOrder": "BY_START_TIME",
- *   "queryDuration": {
- *       "start": "2018-12-20 2131",
- *       "end": "2018-12-20 2146",
- *       "step": "MINUTE"
- *   },
- *   "paging": {
- *       "pageNum": 1,
- *       "pageSize": 20,
- *       "needTotal": true
- *   }
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 export const Traces = {
   variable: '$condition: TraceQueryCondition',
   query: `
