62c207e4f677e4b865eec4dcfffad9e7c95066fb
Allen Wang <Allen.Wang.123@outlook.com>
2019-06-19 21:32:08 +0800

    Update: remove login page (#107)

--- src/router.ts
+++ src/router.ts
@@ -17,7 +17,7 @@
 
 import Vue from 'vue';
 import Router from 'vue-router';
-import Login from './views/containers/login.vue';
+// import Login from './views/containers/login.vue';
 import Index from './views/containers/index.vue';
 import Dashboard from './views/containers/dashboard.vue';
 import Trace from './views/containers/trace.vue';
@@ -33,11 +33,11 @@ const router = new Router({
   base: process.env.BASE_URL,
   linkActiveClass: 'active',
   routes: [
-    {
-      path: '/login',
-      component: Login,
-      meta: { login: true },
-    },
+    // {
+    //   path: '/login',
+    //   component: Login,
+    //   meta: { login: true },
+    // },
     {
       path: '/',
       component: Index,
@@ -71,15 +71,16 @@ router.beforeEach((to, from, next) => {
     }
     w.axiosCancel = [];
   }
-  if (to.meta.login && (token === null || token === 'guest')) {
-    next();
-  } else if (token === null || token === 'guest') {
-    next('/login');
-  } else if (to.meta.login) {
-    next(from.path);
-  } else {
-    next();
-  }
+  // if (to.meta.login && (token === null || token === 'guest')) {
+  //   next();
+  // } else if (token === null || token === 'guest') {
+  //   next('/login');
+  // } else if (to.meta.login) {
+  //   next(from.path);
+  // } else {
+  //   next();
+  // }
+  next();
 });
 
 export default router;
