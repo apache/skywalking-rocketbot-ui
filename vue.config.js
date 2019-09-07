e5dd5042077a8a049041225ee4f5b6d1adaadbeb
x22x22 <rootshellexp@gmail.com>
2019-09-08 00:07:27 +0800

    fix issuse https://github.com/apache/skywalking/issues/3432

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/graphql': {
-        target: 'http://127.0.0.1:8080',
+        target: 'http://127.0.0.1:12800',
         changeOrigin: true,
       },
     },
