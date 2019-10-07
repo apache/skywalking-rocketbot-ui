34b55dd4670fd0e33c382d05c99a230f0514be71
yi.liang <yi.liang@zhangmen.cn>
2019-10-07 22:06:04 +0800

    the jvm-maxHeap & jvm-maxNonHeap is -1, Free is no value

--- src/store/modules/dashboard/source/index.ts
+++ src/store/modules/dashboard/source/index.ts
@@ -71,8 +71,8 @@ export interface State {
   instanceCPU: { cpu: number[]; };
   instanceGC: { oldGC: number[], youngGC: number[] };
   instanceGCCount: { oldGC: number[], youngGC: number[] };
-  instanceHeap: { Value: number[], Free: number[] };
-  instanceNonheap: { Value: number[], Free: number[] };
+  instanceHeap: { Value: number[], Free: number[], ValueUnlimited: boolean };
+  instanceNonheap: { Value: number[], Free: number[], ValueUnlimited: boolean };
   instanceClrHeap: { Value: number[] };
   instanceClrCPU: { cpu: number[]; };
   instanceClrGC: { clrGCGen0: number[], clrGCGen1: number[], clrGCGen2: number[]};
