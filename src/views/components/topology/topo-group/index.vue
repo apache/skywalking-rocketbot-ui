b6261bbb58977b0aeeab06d91722b2d250c54f10
Wang Yao <yao@wangdeMacBook-Pro.local>
2020-03-09 20:53:40 +0800

    Update

--- src/views/components/topology/topo-group/index.vue
+++ src/views/components/topology/topo-group/index.vue
@@ -18,17 +18,17 @@
   <div class="topo-group">
     <div v-for="i in rocketTopoGroup.groups">
       <GroupItem
+      :servicesMap="servicesMap"
       :active="rocketTopoGroup.groupId === i.id"
       :key="i.id"
-      @delete="handleSelectGroup(i.id)"
-      @select="handleSelectGroup(i.id)"
+      @delete="handleDeleteGroup(i.id)"
+      @select="handleSelectGroup"
       :data="i"/>
     </div>
     <div>
-      <div class="group-item default" @click="handleSelectGroup('all')" :class="{'active': rocketTopoGroup.groupId === 'all'}"><span>Default</span></div>
+      <div class="group-item default mb-10" @click="handleSelectGroup('all')" :class="{'active': rocketTopoGroup.groupId === 'all'}"><span>Default</span></div>
     </div>
     <CreateGroup/>
-    <GroupServices/>
   </div>
 </template>
 <script lang="ts">
@@ -38,12 +38,12 @@
   import { State as TopoGroupState } from '@/store/modules/topology/group';
   import CreateGroup from './create-group.vue';
   import GroupItem from './group-item.vue';
-  import GroupServices from './group-services.vue';
+  import Axios, { AxiosResponse } from 'axios';
+  
   @Component({
     components: {
       CreateGroup,
       GroupItem,
-      GroupServices,
     },
   })
   export default class TopoGroup extends Vue {
@@ -55,7 +55,8 @@
     @Mutation('rocketTopoGroup/DELETE_GROUP') private DELETE_GROUP: any;
     @Mutation('rocketTopoGroup/SELECT_GROUP') private SELECT_GROUP: any;
     @Action('rocketTopo/FILTER_TOPO') private FILTER_TOPO: any;
-
+  
+    private servicesMap = [];
     private handleDeleteGroup(id: string) {
       const r = confirm('Do you want to delete this group!');
       if (r === true) {
@@ -66,8 +67,27 @@
       this.SELECT_GROUP(id);
       this.FILTER_TOPO({ services: this.services, group: id });
     }
+    private fetchData() {
+      Axios.post('/graphql', {
+        query: `
+      query queryServices($duration: Duration!) {
+        services: getAllServices(duration: $duration) {
+          key: id
+          label: name
+        }
+      }`,
+        variables: {
+          duration: this.durationTime,
+        },
+      }).then((res: AxiosResponse) => {
+        this.servicesMap = res.data.data.services
+          ? res.data.data.services
+          : [];
+      });
+    }
     private created() {
       this.INIT_GROUPS();
+      this.fetchData();
     }
   }
 </script>
