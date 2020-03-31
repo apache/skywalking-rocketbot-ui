9d988db954ba721f79dcb1680c38fc2b8bfae463
Qiuxia Fan <fine0830@outlook.com>
2020-03-31 20:57:19 +0800

    Feat: Format header && Remove alarm key (#277)
    * feat: rm alarm key
    
    * fix:click charts error
    
    * style: format code

--- src/views/components/dashboard/charts/chart-bar.vue
+++ src/views/components/dashboard/charts/chart-bar.vue
@@ -1,43 +1,38 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+<!-- Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. -->
 
 <template>
-  <RkEcharts ref="chart" :option="option" :autoResize="true"/>
+  <RkEcharts ref="chart" :option="option" :autoResize="true" />
 </template>
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
+  import { Vue, Component, Prop } from 'vue-property-decorator';
 
-@Component
-export default class ChartBar extends Vue {
+  @Component
+  export default class ChartBar extends Vue {
     @Prop() private data!: any;
     @Prop() private intervalTime!: any;
     public resize() {
       const chart: any = this.$refs.chart;
       chart.myChart.resize();
     }
-  get option() {
-    const temp: any = [];
-    const keys = Object.keys(this.data || {});
-    keys.forEach((i: any, index: number) => {
-      temp.push({
-          data: this.data[i].map((item: any, itemIndex: number) => [
-            this.intervalTime[itemIndex],
-            item,
-          ]),
+    get option() {
+      const temp: any = [];
+      const keys = Object.keys(this.data || {});
+      keys.forEach((i: any, index: number) => {
+        temp.push({
+          data: this.data[i].map((item: any, itemIndex: number) => [this.intervalTime[itemIndex], item]),
           name: i,
           type: 'bar',
           symbol: 'none',
@@ -47,57 +42,50 @@ export default class ChartBar extends Vue {
             width: 1.5,
             type: 'dotted',
           },
+        });
       });
-    });
-    return {
-      color: [
-        '#3f96e3',
-        '#3f96e3',
-        '#3fa9e1',
-        '#3fbcde',
-        '#3fcfdc',
-        '#3fe1da',
-      ],
-      tooltip: {
-        trigger: 'axis',
-        backgroundColor: 'rgb(50,50,50)',
-        textStyle: {
-          fontSize: 13,
+      return {
+        color: ['#3f96e3', '#3f96e3', '#3fa9e1', '#3fbcde', '#3fcfdc', '#3fe1da'],
+        tooltip: {
+          trigger: 'axis',
+          backgroundColor: 'rgb(50,50,50)',
+          textStyle: {
+            fontSize: 13,
+          },
         },
-      },
-      legend: {
-        show: keys.length === 1 ? false : true,
-        icon: 'circle',
-        top: 0,
-        left: 0,
-        itemWidth: 12,
-      },
-      grid: {
-        top: keys.length === 1 ? 15 : 40,
-        left: 0,
-        right: 10,
-        bottom: 5,
-        containLabel: true,
-      },
-      xAxis: {
-        type: 'category',
-        axisTick: {
-          lineStyle: { color: '#c1c5ca41' },
-          alignWithLabel: true,
+        legend: {
+          show: keys.length === 1 ? false : true,
+          icon: 'circle',
+          top: 0,
+          left: 0,
+          itemWidth: 12,
         },
-        splitLine: { show: false },
-        axisLine: { lineStyle: { color: 'rgba(0,0,0,0)' } },
-        axisLabel: { color: '#9da5b2', fontSize: '11' },
-      },
-      yAxis: {
-        type: 'value',
-        axisLine: { show: false },
-        axisTick: { show: false },
-        splitLine: { lineStyle: { color: '#c1c5ca41', type: 'dashed' } },
-        axisLabel: { color: '#9da5b2', fontSize: '11' },
-      },
-      series: temp,
-    };
+        grid: {
+          top: keys.length === 1 ? 15 : 40,
+          left: 0,
+          right: 10,
+          bottom: 5,
+          containLabel: true,
+        },
+        xAxis: {
+          type: 'category',
+          axisTick: {
+            lineStyle: { color: '#c1c5ca41' },
+            alignWithLabel: true,
+          },
+          splitLine: { show: false },
+          axisLine: { lineStyle: { color: 'rgba(0,0,0,0)' } },
+          axisLabel: { color: '#9da5b2', fontSize: '11' },
+        },
+        yAxis: {
+          type: 'value',
+          axisLine: { show: false },
+          axisTick: { show: false },
+          splitLine: { lineStyle: { color: '#c1c5ca41', type: 'dashed' } },
+          axisLabel: { color: '#9da5b2', fontSize: '11' },
+        },
+        series: temp,
+      };
+    }
   }
-}
 </script>
--- src/views/components/dashboard/charts/chart-edit.vue
+++ src/views/components/dashboard/charts/chart-edit.vue
@@ -1,75 +1,91 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+<!-- Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. -->
 
 <template>
   <div class="rk-chart-edit">
     <div class="rk-chart-edit-container">
       <div class="flex-h mb-5">
-        <div class="title grey sm">{{$t('title')}}:</div>
-        <input type="text" class="rk-chart-edit-input long" :value="i.t" @change="EDIT_COMP({index,type:'t',value:$event.target.value})">
+        <div class="title grey sm">{{ $t('title') }}:</div>
+        <input
+          type="text"
+          class="rk-chart-edit-input long"
+          :value="i.t"
+          @change="EDIT_COMP({ index, type: 't', value: $event.target.value })"
+        />
       </div>
       <div class="flex-h mb-5">
-        <div class="title grey sm">{{$t('width')}}:</div>
-        <input type="number" min="1" max="12" class="rk-chart-edit-input long" :value="i.w" @change="EDIT_COMP({index,type:'w',value:$event.target.value})">
+        <div class="title grey sm">{{ $t('width') }}:</div>
+        <input
+          type="number"
+          min="1"
+          max="12"
+          class="rk-chart-edit-input long"
+          :value="i.w"
+          @change="EDIT_COMP({ index, type: 'w', value: $event.target.value })"
+        />
       </div>
       <div class="flex-h">
-        <div class="title grey sm">{{$t('height')}}:</div>
-        <input type="number" min="1"  class="rk-chart-edit-input long" :value="i.h" @change="EDIT_COMP({index,type:'h',value:$event.target.value})">
+        <div class="title grey sm">{{ $t('height') }}:</div>
+        <input
+          type="number"
+          min="1"
+          class="rk-chart-edit-input long"
+          :value="i.h"
+          @change="EDIT_COMP({ index, type: 'h', value: $event.target.value })"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import { State, Getter, Mutation } from 'vuex-class';
-import { Component, Prop } from 'vue-property-decorator';
+  import Vue from 'vue';
+  import { State, Getter, Mutation } from 'vuex-class';
+  import { Component, Prop } from 'vue-property-decorator';
 
-@Component
-export default class ChartEdit extends Vue {
-  @Mutation('EDIT_COMP') private EDIT_COMP: any;
-  @Prop() private i!: any;
-  @Prop() private index!: number;
-  @Prop() private intervalTime!: any;
-}
+  @Component
+  export default class ChartEdit extends Vue {
+    @Mutation('EDIT_COMP') private EDIT_COMP: any;
+    @Prop() private i!: any;
+    @Prop() private index!: number;
+    @Prop() private intervalTime!: any;
+  }
 </script>
 <style lang="scss">
-.rk-chart-edit{
-  margin: 0 -10px;
-  height: 100%;
-  overflow: auto;
-}
-
-.rk-chart-edit-container{
-  padding: 7px 5px;
-  border: 1px dashed rgba(196, 200, 225, .5);
-  border-radius: 4px;
-  height: 100%;
-  .title{
-    width: 45px;
-    flex-shrink: 0;
+  .rk-chart-edit {
+    margin: 0 -10px;
+    height: 100%;
+    overflow: auto;
   }
-}
-.rk-chart-edit-input{
-  border: 0;
-  outline: 0;
-  padding: 4px 10px;
-  border-radius: 3px;
-  // border: 1px solid #c1c5ca41;
-  background-color: rgba(196, 200, 225, 0.2);
-}
+
+  .rk-chart-edit-container {
+    padding: 7px 5px;
+    border: 1px dashed rgba(196, 200, 225, 0.5);
+    border-radius: 4px;
+    height: 100%;
+    .title {
+      width: 45px;
+      flex-shrink: 0;
+    }
+  }
+  .rk-chart-edit-input {
+    border: 0;
+    outline: 0;
+    padding: 4px 10px;
+    border-radius: 3px;
+    // border: 1px solid #c1c5ca41;
+    background-color: rgba(196, 200, 225, 0.2);
+  }
 </style>
