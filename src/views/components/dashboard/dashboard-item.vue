163059e80cef4fe2f02b53cc935ef5e27f3ce642
Qiuxia Fan <fine0830@outlook.com>
2020-05-29 13:51:41 +0800

    Bugfix: dashboard common selectors (#301)
    * fix: dashboard common selectors
    
    * fix: typo
    
    * fix: name

--- src/views/components/dashboard/dashboard-item.vue
+++ src/views/components/dashboard/dashboard-item.vue
@@ -41,7 +41,6 @@ limitations under the License. -->
       </div>
     </div>
     <rk-sidebox
-      class="instance-dependency"
       width="70%"
       :fixed="true"
       :title="$t('editConfig')"
@@ -237,16 +236,8 @@ limitations under the License. -->
       }
     }
 
-    @Watch('rocketOption.currentInstance')
-    private watchCurrentInstance() {
-      this.chartRender();
-    }
-    @Watch('rocketOption.currentEndpoint')
-    private watchCurrentEndpoint() {
-      this.chartRender();
-    }
-    @Watch('rocketOption.currentDatabase')
-    private watchCurrentDatabase() {
+    @Watch('rocketOption.updateDashboard')
+    private watchCurrentSelectors() {
       this.chartRender();
     }
     @Watch('durationTime')
