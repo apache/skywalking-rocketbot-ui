c4495e3d7b8f160a2661340c65ec3235c5705c98
Qiuxia Fan <fine0830@outlook.com>
2021-01-24 19:44:24 +0800

    fix: config

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/graphql': {
-        target: `${process.env.SW_PROXY_TARGET || 'http://hz.zhangwei.asia:38080'}`,
+        target: `${process.env.SW_PROXY_TARGET || 'http://127.0.0.1:12800'}`,
 
         changeOrigin: true,
       },
