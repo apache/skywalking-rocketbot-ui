cb73a48aaafc01aa3bac830cc1a60e25f69ccc43
hailin0 <hailin0@yeah.net>
2021-03-10 14:47:45 +0800

    Fix log pages tags column not updated (#443)
    Co-authored-by: 吴晟 Wu Sheng <wu.sheng@foxmail.com>

--- src/views/components/log/log-table/log-service-item.vue
+++ src/views/components/log/log-table/log-service-item.vue
@@ -41,12 +41,11 @@ limitations under the License. -->
     @Prop() private data: any;
     @Prop() private noLink!: any;
     private columns = ServiceLogConstants;
-    private tags: string = '';
-    private created() {
+    private get tags() {
       if (!this.data.tags) {
-        return;
+        return '';
       }
-      this.tags = String(this.data.tags.map((d: any) => `${d.key}=${d.value}`));
+      return String(this.data.tags.map((d: any) => `${d.key}=${d.value}`));
     }
     private showSelectSpan() {
       this.$eventBus.$emit('HANDLE-SELECT-LOG', this.data);
